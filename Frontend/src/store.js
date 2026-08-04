3f9b04ae433458679a2e9bdd068088d995f8db05
Vishvesh <158436133+Vishvesh-Patil@users.noreply.github.com>
2026-08-04 15:26:53 +0530

    Delete Frontend directory

--- Frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import CartSlice from './slices/CartSlice';
-
-const store = configureStore({
-    reducer: {
-        CartSlice
-    }
-})
-export default store
